f22ff9e0fa482f7f1013f9f95af7f5c533b01bae
Vivek110303 <madhakvivek4301@gmail.com>
2025-07-28 09:53:31 +0530

    Add files via upload

---
+++ Assignment-1/Task-1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a + b
+d = a * b
+e = a - b
+f = a / b
+print("Addition :", c)
+print("Substraction :", e)
+print("Multiplication :", d)
+print("Division :", f)
+
---
+++ Assignment-1/Task-2.py
@@ -0,0 +1,9 @@
+# Step 1: Take first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Step 2: Concatenate first and last name
+full_name = first_name + " " + last_name
+
+# Step 3: Print a personalized greeting message
+print("Hello, " + full_name + "! Welcome to the python program.")
