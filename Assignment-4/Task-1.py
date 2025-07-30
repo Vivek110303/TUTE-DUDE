afccfcf6cf3881a975bc60c2c3b75eb0729a8c29
Vivek110303 <madhakvivek4301@gmail.com>
2025-07-30 20:47:00 +0530

    Delete Assignment-4 directory

--- Assignment-4/Task-1.py
+++
@@ -1,8 +0,0 @@
-try:
-    with open("sample.txt", "r") as file:
-        line_number = 1
-        for line in file:
-            print(f"line {line_number}:{line.strip()}")
-            line_number += 1
-except FileNotFoundError:
-    print("the file sample not found")
